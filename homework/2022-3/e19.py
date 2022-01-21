faeb3d420e45b07e0edbcdab71702bcf695efcac
Aapo Humina <rageidos@gmail.com>
2022-01-21 16:27:18 +0200

    e19 done

---
+++ homework/2022-3/e19.py
@@ -0,0 +1,9 @@
+# Kysytään käyttäjältä positiivinen parillinen kokonaisluku
+print("Anna positiivinen parilllinen kokonaisluku")
+luku = int(input())
+
+# Printatanaan joka toinen luku väliltä 0 - luku
+i = 0
+while i < luku:
+        print(i)
+        i = i + 2
