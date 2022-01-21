1bc23a8fa1e080840fc8decf65866de27446eb62
Aapo Humina <rageidos@gmail.com>
2022-01-21 14:40:52 +0200

    e06 done

---
+++ homework/2022-3/e06.py
@@ -0,0 +1,19 @@
+# Kysy ensimmäinen luku
+print("Anna luku 1:")
+luku1 = int(input())
+
+# Kysy toinen luku
+print("Anna luku 2:")
+luku2 = int(input())
+
+# Kysy kolmas luku
+print("Anna luku 3:")
+luku3 = int(input())
+
+# Summataan luvut yhteen
+summa = luku1 + luku2 + luku3
+
+# Lasketaan keskiarvo ja printataan se
+keskiarvo = summa / 3
+print("Keskiarvo on: ")
+print(keskiarvo)
