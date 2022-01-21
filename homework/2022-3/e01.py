28f868e0df78f0d7ce3ebd49228968391fc0338f
Aapo Humina <rageidos@gmail.com>
2022-01-21 13:47:06 +0200

    e01 done

--- homework/2022-3/e01.py
+++ homework/2022-3/e01.py
@@ -1,1 +1,22 @@
-print("Aapo Humina")
+# Luodaan kilpikonna
+import turtle
+kilpikonna = turtle.Turtle()
+kilpikonna.shape("turtle")
+
+# Mene eteenpäin 50 pikseliä
+kilpikonna.forward(50)
+
+# Käänny vasemmalle 90 astetta
+kilpikonna.left(90)
+
+# Mene eteenpäin 50 pikseliä
+kilpikonna.forward(50)
+
+# Käänny vasemmalle 90 astetta
+kilpikonna.left(90)
+
+# Toteuta tänne toiminnallisuus jossa kilpikonna piirtää neliön
+# ja palaa alkuperäiseen pisteeseen.
+kilpikonna.forward(50)
+kilpikonna.left(90)
+kilpikonna.forward(50)
