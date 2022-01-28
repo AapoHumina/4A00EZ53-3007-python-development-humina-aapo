7cbc75db797e60f9dda96e42d89495871103d77d
Aapo Humina <rageidos@gmail.com>
2022-01-28 12:58:00 +0200

    e01 done

--- homework/2022-4/e01.py
+++ homework/2022-4/e01.py
@@ -1,1 +1,28 @@
-print("This is exercise 01")
+# Nämä ovat muuttujat a ja b
+a = "hello"
+b = "world"
+
+#print("hello world")
+print("hello world") #Tämä printtaa hei maailma
+
+# Nämä printtavat 
+# muuttujia a ja b
+# eri tavoin
+print(a)
+print(a,b)
+print(a, b, sep=":")
+print(a, b, sep=":", end =";")
+print("\n")
+
+"""
+Tämä printtaa paljon välilyöntejä
+"""
+print(a, "\n", "\n")
+print(b)
+
+print(a, b, sep = "")
+
+print(a, end = "")
+print(b)
+
+help(print)
