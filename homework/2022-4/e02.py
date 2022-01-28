97439b1081c472f23f7b1d3a267bf119e2272072
Aapo Humina <rageidos@gmail.com>
2022-01-28 13:10:36 +0200

    e02 done

--- homework/2022-4/e02.py
+++ homework/2022-4/e02.py
@@ -1,1 +1,13 @@
-print("This is exercise 02")
+luku = 5
+leiju_luku = 5.5
+sana = "hello"
+totuus_arvo = True
+
+print(luku, type(luku))
+print(leiju_luku, type(leiju_luku))
+print(sana, type(sana))
+print(totuus_arvo, type(totuus_arvo))
+
+print(bool(0))
+print(bool(1))
+print(bool(sana))
