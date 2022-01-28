c0fb69292fce2e034e87f75e702a7ceed993116d
Aapo Humina <rageidos@gmail.com>
2022-01-28 14:35:50 +0200

    e04 done

--- homework/2022-4/e02.py
+++ homework/2022-4/e02.py
@@ -7,7 +7,3 @@ print(luku, type(luku))
 print(leiju_luku, type(leiju_luku))
 print(sana, type(sana))
 print(totuus_arvo, type(totuus_arvo))
-
-print(bool(0))
-print(bool(1))
-print(bool(sana))
---
+++ homework/2022-4/e04.py
@@ -0,0 +1,37 @@
+luku = str(5)
+leiju_luku = str(5.5)
+sana = "hello"
+totuus_arvo = str(True)
+
+print(luku, type(luku))
+print(leiju_luku, type(leiju_luku))
+print(sana, type(sana))
+print(totuus_arvo, type(totuus_arvo))
+
+print(luku + leiju_luku)
+print(float(luku) + float(leiju_luku))
+print(bool(luku) + bool(leiju_luku))
+
+f_luku = int(10.6)
+print(f_luku)
+
+f_luku2 = 10.6
+print(round(f_luku2))
+
+int_luku = float(10)
+print(int_luku)
+
+int_luku1 = 5
+int_luku2 = 6
+sum = int_luku1 + int_luku2
+print(sum, type(sum))
+
+int_luku3 = 6
+f_luku3 = 6.6
+sum1 = int_luku3 + f_luku3
+print(sum1, type(sum1))
+
+int_luku4 = 6
+int_luku5 = 8
+division = int_luku4 / int_luku5
+print(division, type(division))
