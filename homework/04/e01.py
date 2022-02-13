434fba3daebc91192e580bc5a1266a1da4d210fc
Aapo Humina <rageidos@gmail.com>
2022-02-13 17:19:14 +0200

    added folder 4

---
+++ homework/04/e01.py
@@ -0,0 +1,24 @@
+import random
+
+def get_lotto_numbers(amount, min, max):
+    lotto_numbers = set()
+    while len(lotto_numbers) < amount:
+        lotto_numbers.add(random.randint(min,max))
+    return lotto_numbers
+
+def get_lotto_numbers2(amount, min, max):
+    lotto_numbers2 = []
+    for num in range(amount):
+        num = random.randint(min, max)
+        lotto_numbers2.append(num)
+    return lotto_numbers2
+
+
+
+
+
+
+
+
+print(get_lotto_numbers(7, 1, 40))
+print(get_lotto_numbers2(7, 1, 20))
