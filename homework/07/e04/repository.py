b98a48b641c71ac46f5f64c582be7893f30c5eec
Aapo Humina <rageidos@gmail.com>
2022-03-11 14:22:32 +0200

    07/e04 Complete

---
+++ homework/07/e04/repository.py
@@ -0,0 +1,17 @@
+def read_database():
+   db_string = open("database.txt", "r")
+   return db_string.read()
+
+def save_to_database(fname, lname):
+   f = open("database.txt", "r")
+   for x in f:
+      last_line=(x)
+   id_number= int(last_line[0])+1
+   db_string = open("database.txt", "a")
+   db_string.write(f"\n{id_number},{fname},{lname}")
+
+
+
+
+save_to_database("James", "Bond")
+print(read_database())
